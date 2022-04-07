27bd5b75e322561773aa9d0dbf42dc2f90eb78b0
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-06 23:53:01 -0400

    trying to run on heroku

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,13 @@ app.use("/api/search", require("./routes/searchRoute"));
 //serve frontend
 if (process.env.NODE_ENV === "production") {
   //set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static("../frontend/build"));
 
+  /*
   app.get("*", (req, res) =>
     res.sendFile(__dirname, "../", "frontend", "build", "index.html")
   );
+  */
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
