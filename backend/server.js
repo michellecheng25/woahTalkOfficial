bfa9c4524e59742086ac626323d308724ebc3e39
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-06 23:23:22 -0400

    fixed typo

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.httml")
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
   );
 }
 
