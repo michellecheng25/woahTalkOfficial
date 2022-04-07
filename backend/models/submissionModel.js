654b4df4a5b8785c5f6f77f036941a34711ced89
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-07 01:45:01 -0400

    made git case sensitive

---
+++ backend/models/submissionModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const submissionSchema = mongoose.Schema(
+  {
+    assignmentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Assignment",
+    },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    content: {
+      type: String,
+    },
+    upload: {
+      type: String,
+    },
+    grade: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Submissionn", submissionSchema);
