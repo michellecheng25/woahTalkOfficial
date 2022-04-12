d2f4f1657a78ce3376cb290ddb2925a6eb87e155
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-12 14:41:08 -0400

    reverse edits to coursebox and implemented likes

--- frontend/src/components/CourseItem.jsx
+++ frontend/src/components/CourseItem.jsx
@@ -1,46 +1,9 @@
 import { Link, useNavigate, Navigate } from "react-router-dom";
 import { useState, useContext } from "react";
 import "./courseItem.css";
-import axios from "axios";
-import { toast } from "react-toastify";
-import UserContext from "../context/users/UserContext";
-import { joinCourse, leaveCourse } from "../context/users/UserActions";
 
-function CourseItem({ course, userCourseList }) {
+function CourseItem({ course }) {
   const courseLink = "/courses/" + course._id;
-  const isJoined = userCourseList.includes(course._id);
-  const [join, setJoin] = useState(isJoined);
-  const token = JSON.parse(localStorage.getItem("token"));
-  const { user, dispatch } = useContext(UserContext);
-
-  const onJoin = () => {
-    setJoin(!join);
-    joinTheCourse();
-  };
-
-  const joinTheCourse = async () => {
-    if (user) {
-      const userAction = join ? "leave" : "join";
-      const followAction = {
-        action: userAction,
-      };
-      console.log(followAction);
-
-      axios
-        .post("api/courses/" + course._id + "/join", followAction, {
-          headers: { Authorization: `Bearer ${token}` },
-        })
-        .then(() => {
-          console.log(join);
-        })
-        .catch((error) => toast.error(error.response.data));
-
-      if (userAction === "join") joinCourse(course._id, dispatch);
-      else {
-        leaveCourse(course._id, dispatch);
-      }
-    } else console.log("cant join");
-  };
 
   return (
     <div className="courseBox">
@@ -50,17 +13,11 @@ function CourseItem({ course, userCourseList }) {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
-      </Link>
 
-      <div onClick={onJoin}>
-        <h4 className="join-btn">{join ? "Leave" : "Join"}</h4>
-      </div>
+        <h4 className="join-btn">View Course</h4>
+      </Link>
     </div>
   );
 }
-
-CourseItem.defaultProps = {
-  userCourseList: [],
-};
 
 export default CourseItem;
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import { useState, useContext } from "react";
 import "./post.css";
 import { Link, useNavigate } from "react-router-dom";
 import { FcLikePlaceholder, FcLike } from "react-icons/fc";
 import { GoComment } from "react-icons/go";
+import UserContext from "../context/users/UserContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Post({ post }) {
   const userLink = "/profile/" + post.userId.username;
   const postLink = "/posts/" + post._id;
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
+  const isLiked = post.likes.includes(user._id);
+  const [like, setLike] = useState(isLiked);
+  const token = JSON.parse(localStorage.getItem("token"));
+
+  const onLike = () => {
+    setLike(!like);
+    likePost();
+  };
+
+  const likePost = () => {
+    const userAction = like ? "unlike" : "like";
+    const likeAction = {
+      action: userAction,
+    };
+    console.log(likeAction);
+
+    axios
+      .post("api/posts/" + post._id + "/like", likeAction, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => toast.error(error.response.data));
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -33,8 +63,8 @@ function Post({ post }) {
           )}
         </div>
         <div className="postInteractions">
-          <div className="likeBtn">
-            <FcLike size={30} />
+          <div className="likeBtn" onClick={onLike}>
+            {like ? <FcLike size={30} /> : <FcLikePlaceholder size={30} />}
           </div>
 
           <div
