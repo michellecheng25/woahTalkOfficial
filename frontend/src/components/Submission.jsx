f632520a7b919481a570acd40c4bc9fcf46b8a99
Michelle Cheng <michellecheng.2017@gmail.com>
2022-05-16 12:36:35 -0400

    edited submission user link to lead to their profile

--- frontend/src/components/Submission.jsx
+++ frontend/src/components/Submission.jsx
@@ -44,60 +44,95 @@ function Submission({ submission, totalPoints, courseId, assignmentId }) {
     >
       <div className="style.submissionWrapper">
         <div className="user-info">
-          <Link to="/" className="profile-pic">
-            <img src={submission.creatorId.profilePicture} style={{ width: "40px", height: "40px"}}alt="profile-pic" />
+          <Link
+            to={`/profile/${submission.creatorId.username}`}
+            className="profile-pic"
+          >
+            <img
+              src={submission.creatorId.profilePicture}
+              style={{ width: "40px", height: "40px" }}
+              alt="profile-pic"
+            />
           </Link>
           <div>
-            <Link to="/">
-              <h2  style= {{fontSize: "19px", color: "black !important",}}>{submission.creatorId.username}</h2>
+            <Link to={`/profile/${submission.creatorId.username}`}>
+              <h2 style={{ fontSize: "19px", color: "black !important" }}>
+                {submission.creatorId.username}
+              </h2>
             </Link>
-            <h4 style={{fontSize: "13px", fontWeight: "normal", marginTop: "3px"}}>{dateTimeConversion(submission.updatedAt)}</h4>
+            <h4
+              style={{
+                fontSize: "13px",
+                fontWeight: "normal",
+                marginTop: "3px",
+              }}
+            >
+              {dateTimeConversion(submission.updatedAt)}
+            </h4>
           </div>
         </div>
 
-        <div className="content" style={{ whiteSpace: "pre-wrap", fontSize: "17px", marginTop: "10px" }}>
+        <div
+          className="content"
+          style={{
+            whiteSpace: "pre-wrap",
+            fontSize: "17px",
+            marginTop: "10px",
+          }}
+        >
           {submission.content}
         </div>
-      
-      <div style={{height: "45px", display:"flex", justifyContent: "flex-end",}}>
-        {totalPoints !== 0 && (
-          <div style={{display: "flex"}}>
-            <p style= {{marginTop: "13px", fontWeight:"bold"}}>Grade: </p>
-            <div style={{ display: "inline-block", padding: "5px", marginRight: "10px"}}>
-              <input
-                name="name"
+
+        <div
+          style={{
+            height: "45px",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          {totalPoints !== 0 && (
+            <div style={{ display: "flex" }}>
+              <p style={{ marginTop: "13px", fontWeight: "bold" }}>Grade: </p>
+              <div
                 style={{
-                  height: "40px",
-                  width: "43px",
-                  fontSize: "20px",
-                  padding: "3px", 
-                  borderRadius:"8px"
+                  display: "inline-block",
+                  padding: "5px",
+                  marginRight: "10px",
                 }}
-                value={grade}
-                onChange={(e) => {
-                  setGrade(e.target.value);
-                }}
-              />
-              <span style= {{fontWeight:"bold"}}> / {totalPoints}</span>
+              >
+                <input
+                  name="name"
+                  style={{
+                    height: "40px",
+                    width: "43px",
+                    fontSize: "20px",
+                    padding: "3px",
+                    borderRadius: "8px",
+                  }}
+                  value={grade}
+                  onChange={(e) => {
+                    setGrade(e.target.value);
+                  }}
+                />
+                <span style={{ fontWeight: "bold" }}> / {totalPoints}</span>
+              </div>
             </div>
-          </div>
-        )}
-        {totalPoints !== 0 && (
-          <button 
-            style= {{ 
-              display: "flex", 
-              justifyContent: "flex-end",
-              borderRadius: "10px",
-            }} 
-            type="Grade" 
-            className="createCourseContentBtn">
-            Submit
-          </button>
-        )}
+          )}
+          {totalPoints !== 0 && (
+            <button
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                borderRadius: "10px",
+              }}
+              type="Grade"
+              className="createCourseContentBtn"
+            >
+              Submit
+            </button>
+          )}
+        </div>
       </div>
-
-      </div>
-
     </form>
   );
 }
@@ -107,5 +142,5 @@ const style = {
     position: "relative",
     display: "flex",
   },
-}
+};
 export default Submission;
